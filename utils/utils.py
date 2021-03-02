218df4bc42a8414241cc3b17f27fc9aa9d14ad0f
franco <franco@flydreamers.com>
2021-03-01 22:46:00 -0300

    Comments

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,12 @@
-def user_answer_is_yes(input_user: str):
+def user_answer_is_yes(input_user: str) -> bool:
     """
+    Check if the user input is affirmative
     
-    :param input_user:
-    :return:
+    Arguments:
+        input_user (str): The user input
+    
+    Returns:
+        bool
     """
     if input_user.isnumeric() or (input_user.lower() not in ["yes", "ye", "y"]):
         return False
